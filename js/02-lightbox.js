c2f1263252aa6a7c2832bdff88d9531d555ca49b
OlhaZaliubovskaya <110026359+OlhaZaliubovskaya@users.noreply.github.com>
2022-12-12 15:51:37 +0200

    lightbox
     homework -2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,21 @@
-
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML(
+    "afterbegin",
+    galleryItems
+        .map(
+            (galleryItems) =>
+                `<a class="gallery__item" href="${galleryItems.original}">
+  <img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}" />
+</a>`
+        )
+        .join("")
+);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+
+});
